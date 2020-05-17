6dfed856fe758bc92db056d14fd4b866957ee548
Ben Reader <breader@vigilant.it>
2020-05-17 21:43:32 +1000

    added additional argumentcompleter

--- Intune.HV.Tools/Intune.HV.Tools.psm1
+++ Intune.HV.Tools/Intune.HV.Tools.psm1
@@ -1,20 +1,22 @@
 #region Get public and private function definition files.
-$Public  = @(Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue)
+$Public = @(Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue)
 $Private = @(Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue)
-$cfg = get-content "$env:USERPROFILE\.hvtoolscfgpath" -ErrorAction SilentlyContinue
+$cfg = Get-Content "$env:USERPROFILE\.hvtoolscfgpath" -ErrorAction SilentlyContinue
 if ($cfg) {
-    $script:hvConfig = (get-content -Path  -raw -ErrorAction SilentlyContinue | ConvertFrom-Json -Depth 20) ?? $null
+    $script:hvConfig = if (Get-Content -Path $cfg -raw -ErrorAction SilentlyContinue) {
+        Get-Content -Path $cfg -raw -ErrorAction SilentlyContinue | ConvertFrom-Json
+    }
+    else {
+        $script:hvConfig = $null
+    }
 }
 #endregion
 #region Dot source the files
-foreach ($import in @($Public + $Private))
-{
-    try
-    {
+foreach ($import in @($Public + $Private)) {
+    try {
         . $import.FullName
     }
-    catch
-    {
+    catch {
         Write-Error -Message "Failed to import function $($import.FullName): $_"
     }
 }
@@ -26,8 +28,25 @@ $clientFinder = {
         $stringMatch
     )
     if ($script:hvConfig) {
-        $script:hvConfig.tenantConfig | Where-Object {$_.TenantName -like "$stringMatch*"} | Select-Object -ExpandProperty TenantName
+        $script:hvConfig.tenantConfig | Where-Object { $_.TenantName -like "$stringMatch*" } | Select-Object -ExpandProperty TenantName
     }
 }
 Register-ArgumentCompleter -CommandName New-ClientVM -ParameterName client -ScriptBlock $clientFinder
+
+$vLan = {
+    param (
+        $commandName,
+        $parameterName,
+        $stringMatch
+    )
+
+    $result = Get-VMSwitch | Where-Object { $_.Name -like "$stringMatch*" } | Select-Object -ExpandProperty Name
+    if ($result -match " ") {
+        '"{0}"' -f $result
+    }
+    else {
+        $result
+    }
+}
+Register-ArgumentCompleter -CommandName Add-VLanToConfig -ParameterName VSwitchName -ScriptBlock $vLan
 #endregion
