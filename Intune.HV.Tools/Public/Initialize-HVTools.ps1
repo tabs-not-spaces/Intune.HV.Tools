4234d2c87716f274277ed1517c0f2f9101a09e85
Yinghua Zeng [MVP] <28437644+sandytsang@users.noreply.github.com>
2020-07-25 20:05:46 +0300

    Always create $env:USERPROFILE\.hvtoolscfgpath
    When use this function initialize a new folder, it didn't update "$env:USERPROFILE\.hvtoolscfgpath" file content, or recreate this new file, so reads hvconfig.json configuration from wrong folder.

--- Intune.HV.Tools/Public/Initialize-HVTools.ps1
+++ Intune.HV.Tools/Public/Initialize-HVTools.ps1
@@ -22,23 +22,18 @@ function Initialize-HVTools {
         }
         $cfgPath = "$Path\.hvtools\hvconfig.json"
         Write-Host " + Creating $cfgPath.. " -ForegroundColor Cyan -NoNewline
-        if ((Test-Path $cfgPath -ErrorAction SilentlyContinue) -and ($Reset -eq $false)) {
-            Write-Host "$script:tick (Already created - no need to run this again..)" -ForegroundColor Green
-        }
-        else {
-            $initCfg = @{
-                'hvConfigPath' = $cfgPath
-                'images'       = @()
-                "vmPath"       = "$Path\.hvtools\tenantVMs"
-                'vSwitchName'  = $null
-                'vLanId'       = $null
-                'tenantConfig' = @()
-            } | ConvertTo-Json -Depth 20
-            $initCfg | Out-File $cfgPath -Encoding ascii -Force
-            $cfgPath | Out-File "$env:USERPROFILE\.hvtoolscfgpath" -Encoding ascii -Force
-            Write-Host $script:tick -ForegroundColor Green
-            $script:hvConfig = (get-content -Path "$(get-content "$env:USERPROFILE\.hvtoolscfgpath" -ErrorAction SilentlyContinue)" -raw -ErrorAction SilentlyContinue | ConvertFrom-Json)
-        }
+        $initCfg = @{
+            'hvConfigPath' = $cfgPath
+            'images'       = @()
+            "vmPath"       = "$Path\.hvtools\tenantVMs"
+            'vSwitchName'  = $null
+            'vLanId'       = $null
+            'tenantConfig' = @()
+        } | ConvertTo-Json -Depth 20
+        $initCfg | Out-File $cfgPath -Encoding ascii -Force
+        $cfgPath | Out-File "$env:USERPROFILE\.hvtoolscfgpath" -Encoding ascii -Force
+        Write-Host $script:tick -ForegroundColor Green
+        $script:hvConfig = (get-content -Path "$(get-content "$env:USERPROFILE\.hvtoolscfgpath" -ErrorAction SilentlyContinue)" -raw -ErrorAction SilentlyContinue | ConvertFrom-Json)
     }
     catch {
         Write-Warning $_
